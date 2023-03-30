d63cdcf07d08736e22e720108b4ece78dfec75b7
Pete Puustinen <petri_puustinen@msn.com>
2023-03-30 21:22:40 +0300

    Apps with SQL conns and Flows with SP conns

---
+++ PowerPlatform/Get-CanvasAppsWithSQLConnection.ps1
@@ -0,0 +1,68 @@
+﻿
+# Script lists all canvas apps in every environment having SQL connection with SQL server, database and connected tables (datasources) info
+# Output is saved to CSV file into same directory where script is located
+
+Add-PowerAppsAccount
+
+$apps = $null
+$output = @()
+
+$envs = Get-AdminPowerAppEnvironment
+
+foreach ($env in $envs)
+{
+    $apps += Get-AdminPowerApp -EnvironmentName $env.EnvironmentName
+}
+
+Write-Host "Canvas apps having SQL Server connection(s)"
+
+foreach ($a in $apps)
+{  
+    if ($a.Internal.properties.connectionReferences -ne $null)
+    {
+        $connRefs = $a.Internal.properties.connectionReferences | gm -MemberType NoteProperty
+
+        $sqls = $connRefs | ? { $_.Definition -like "*id=/providers/Microsoft.PowerApps/scopes/admin/apis/shared_sql*" }
+    
+        $cnt = Measure-Object -InputObject $sqls
+
+        if ($cnt.Count -gt 0)
+        {
+            write-host "APP:'$($a.DisplayName)' ($($a.AppName)) - ENV:$($a.EnvironmentName)"
+
+            foreach ($sql in $sqls)
+            {
+                $c = $a.Internal.properties.connectionReferences."$($sql.Name)"
+
+                $isImplicit = "False"
+
+                if ($c.authenticationType -notin ("oAuth", "windowsAuthenticationNonShared"))
+                {
+                    $isImplicit = "True"
+                }
+
+                $out = New-Object PSObject
+                $out | Add-Member -MemberType NoteProperty -Name AppId -Value $a.AppName
+                $out | Add-Member -MemberType NoteProperty -Name DisplayName -Value $a.DisplayName
+                $out | Add-Member -MemberType NoteProperty -Name AppEnvId -Value $a.EnvironmentName
+                $out | Add-Member -MemberType NoteProperty -Name DataSources -Value ([system.String]::Join(",", $c.dataSources))
+                $out | Add-Member -MemberType NoteProperty -Name Endpoints -Value ([system.String]::Join(",", $c.endpoints))
+                $out | Add-Member -MemberType NoteProperty -Name AuthenticationType -Value $c.authenticationType
+                $out | Add-Member -MemberType NoteProperty -Name IsImplicit -Value $isImplicit
+                $out | Add-Member -MemberType NoteProperty -Name IsOnPrem -Value $c.isOnPremiseConnection
+                $out | Add-Member -MemberType NoteProperty -Name AppSharedusers -Value $a.Internal.properties.sharedUsersCount
+                $out | Add-Member -MemberType NoteProperty -Name AppSharedGroups -Value $a.Internal.properties.sharedGroupsCount
+
+                $output  += $out
+            }
+        }  
+    }    
+}
+
+
+
+$output | Export-Csv -NoTypeInformation $PSScriptRoot\appswithsqlconnections.csv -Force
+
+Write-Host
+Write-Host "Output saved to $("$PSScriptRoot\appswithsqlconnections.csv")"
+Write-Host
---
+++ PowerPlatform/Get-FlowsWithSPConnection.ps1
@@ -0,0 +1,54 @@
+﻿
+Add-PowerAppsAccount
+
+$ft = @{Expression={$_.Environment};Label="Environment"}, 
+	    @{Expression={$_.Flow};Label="Flow"},
+        @{Expression={$_.TriggerAction};Label="TriggerAction"},
+        @{Expression={$_.SiteUrl};Label="SiteUrl"},
+        @{Expression={$_.ListId};Label="ListId"},
+        @{Expression={$_.Operation};Label="Operation"}
+
+$output = @()
+
+$envs = Get-AdminPowerAppEnvironment
+
+foreach ($e in $envs)
+{
+    $flows = Get-AdminFlow -EnvironmentName $e.EnvironmentName
+
+    foreach ($f in $flows)
+    {
+        $flow = Get-AdminFlow -EnvironmentName $e.EnvironmentName -FlowName $f.FlowName 
+
+        $spTriggersActions = $flow.Internal.properties.referencedResources | ? { $_.service -eq "sharepoint" }
+    
+        $cnt = ($spTriggersActions | measure).Count
+
+        if ($cnt.Count -gt 0)
+        {
+            foreach ($ta in $spTriggersActions)
+            {
+                $referencers = $ta.referencers
+
+                foreach ($ref in $referencers)
+                {
+                    $out = New-Object PSObject
+                    $out | Add-Member -MemberType NoteProperty -Name Environment -Value $e.DisplayName
+                    $out | Add-Member -MemberType NoteProperty -Name Flow -Value $flow.DisplayName
+                    $out | Add-Member -MemberType NoteProperty -Name TriggerAction -Value $ref.referenceSourceType
+                    $out | Add-Member -MemberType NoteProperty -Name SiteUrl -Value $ta.resource.site
+                    $out | Add-Member -MemberType NoteProperty -Name ListId -Value $ta.resource.list
+                    $out | Add-Member -MemberType NoteProperty -Name Operation -Value $ref.operationId.Substring($ref.operationId.LastIndexOf('/')+1)
+
+                    $output += $out
+        
+                }
+            }
+        }        
+    }
+}
+
+$output | Format-Table $ft -AutoSize
+
+
+
